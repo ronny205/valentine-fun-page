cc6179c3625dc1c760929dde1e2de3fe71b947d6
ronny205 <rohanaiwale99@gmail.com>
2024-02-14 23:25:33 +0530

    valentine fun web page

---
+++ valentinesFun/src/App.jsx
@@ -0,0 +1,35 @@
+import useState from "react";
+
+function App() {
+  const [hovered, setHovered] = useState(false);
+
+  const handleHover = () => {
+    setHovered(true);
+  };
+
+  const handleLeave = () => {
+    setHovered(false);
+  };
+
+  return (
+    <div className="valentine-container">
+      {" "}
+      {/* Apply the class */}
+      <h2>Will you be my valentine?</h2>
+      <div className="buttons-container">
+        {" "}
+        {/* New container for buttons */}
+        <button className="yes-button">Yes</button>
+        <button
+          className={`no-button ${hovered ? "yes-style" : ""}`}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+        >
+          {hovered ? "Yes" : "No"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
